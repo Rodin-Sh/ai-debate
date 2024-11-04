84302c3f1442622e2477cfc168249fe296541d43
Rodin Sh <65271515+Rodin-Sh@users.noreply.github.com>
2024-11-04 00:48:00 -0800

    Update agents.py
    Created image attribute to allow the streamlit chatbot to change between yellow and red icon

--- src/agents.py
+++ src/agents.py
@@ -1,10 +1,12 @@
 from openai import OpenAI
+import streamlit as st
 
 class Agent():
-    def __init__(self, api_key, model, role, prompt, all_messages):
+    def __init__(self, api_key, model, role, image, prompt, all_messages):
         self.api_key = api_key
         self.model = model
         self.role = role
+        self.image = image
 
         self.client = OpenAI()
         all_messages.append({"role": self.role, "content": prompt})
@@ -16,5 +18,6 @@ class Agent():
             messages=all_messages)
             
         all_messages.append({"role": self.role, "content": response.choices[0].message.content})
+        st.session_state.messages.append({"role": self.image, "content": response.choices[0].message.content})
 
         return response.choices[0].message.content
