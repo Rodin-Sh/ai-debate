cb029d5119e7c85236015bae9caba0dd6328475c
Rodin Sh <65271515+Rodin-Sh@users.noreply.github.com>
2024-11-04 01:21:01 -0800

    Added sidebar with user inputs
    More user inputs are:
    - Entering OpenAI API key
    - Selecting OpenAI LLM model

--- src/main.py
+++ src/main.py
@@ -21,34 +21,46 @@ def main():
     
     # Initialization of all variables
     load_dotenv()
-    api_key = os.getenv("API_KEY")
+    # api_key = os.getenv("API_KEY")
     llm_model = "gpt-4o-mini"
     config = load_agent_prompts()
     all_messages = []
 
+    # Set's the title of the web page on the tab navigation bar
     st.set_page_config(page_title='AI Debate')
 
     topic = ""
     st.title("AI Debate")
     
-    topic = st.text_input(label="Debate Topic", placeholder="God's existence")
-    while topic == "":
-        st.write("Waiting on a topic...")
-        time.sleep(1000)
+    with st.sidebar:
+        api_key = st.text_input(label="OpenAI API Key", type="password")
+        while api_key=="":
+            st.write("Waiting on a topic...")
+            st.stop()
+
+        llm_model = st.selectbox("OpenAI Model",
+        ('gpt-4o-mini', 'gpt-4o', 'gpt-4-turbo'))
+
+        topic = st.text_input(label="Debate Topic", placeholder="God's existence")
+        while topic == "":
+            st.write("Waiting on a topic...")
+            st.stop()
 
     st.caption(f'''About "{topic.strip().title()}"''')
 
+    # Loads the believer and disagreer prompt from the agents.yaml file
     believer_prompt = get_agent_prompt(config, "believer", topic)
     disagreer_prompt = get_agent_prompt(config, "disagreer", topic)
 
+    # Initialized variables used to set the colour of icon for streamlit
     believer_image = "assistant"
     disagreer_image = "user"
 
+    # Creating instances of Agent() class
     believer = Agent(api_key, llm_model, "assistant", believer_image, believer_prompt, all_messages)
     disagreer = Agent(api_key, llm_model, "assistant", disagreer_image, disagreer_prompt, all_messages)
 
-    continue_conversation = True
-
+    # Copying all messages from all_messages to st.session_state.messages
     if "messages" not in st.session_state:
         st.session_state["messages"] = []
         for i in range(len(all_messages)):
@@ -59,26 +71,25 @@ def main():
                 })
 
     
-    # Starting from 3rd message because the first two are internal
-    # prompts for the agents
+    # Starting from 3rd message because the first two are internal prompts for the agents
     for i in range(len(st.session_state["messages"][2:])):
         st.chat_message(believer_image if i%2 == 0 else disagreer_image).write(st.session_state.messages[i]["content"])
 
     for i in range(5):
-    # while continue_conversation:
+
+        # The believer replies first
         reply_believer = believer.generate_response(all_messages)
         st.chat_message(believer_image).write(reply_believer)
 
+        # Wait 5 seconds
         time.sleep(5)
 
+        # The disagreer replies afterwards
         reply_disagreer = believer.generate_response(all_messages)
         st.chat_message(disagreer_image).write(reply_disagreer)
 
+        # Wait 5 seconds
         time.sleep(5)
-
-        continue_conversation = False
-        # st.button("Continue Conversation")
-
 
 if __name__ == "__main__":
     main()
