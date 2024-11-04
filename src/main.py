6b89bd840ce6a87f41ab1ad6f7ad1b8b9ba294a2
Rodin Sh <65271515+Rodin-Sh@users.noreply.github.com>
2024-11-04 00:47:42 -0800

    Update main.py
    Added UI to the chatbot using Streamlit

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 import os
 import yaml
 import time
+import streamlit as st
 
 from agents import Agent
 
@@ -25,30 +26,57 @@ def main():
     config = load_agent_prompts()
     all_messages = []
 
-    # topic = input("TOPIC:")
-    topic = "God's existence"
+    topic = ""
+    st.title("AI Debate")
+    
+    topic = st.text_input(label="Debate Topic", placeholder="God's existence")
+    while topic == "":
+        st.write("Waiting on a topic...")
+        time.sleep(100)
+
+    st.caption(f'''About "{topic.strip().title()}"''')
 
     believer_prompt = get_agent_prompt(config, "believer", topic)
     disagreer_prompt = get_agent_prompt(config, "disagreer", topic)
 
-    print(believer_prompt)
-    print(disagreer_prompt)
+    believer_image = "assistant"
+    disagreer_image = "user"
 
-    believer = Agent(api_key, llm_model, "assistant", believer_prompt, all_messages)
-    disagreer = Agent(api_key, llm_model, "assistant", disagreer_prompt, all_messages)
+    believer = Agent(api_key, llm_model, "assistant", believer_image, believer_prompt, all_messages)
+    disagreer = Agent(api_key, llm_model, "assistant", disagreer_image, disagreer_prompt, all_messages)
 
-    for i in range(3):
+    continue_conversation = True
+
+    if "messages" not in st.session_state:
+        st.session_state["messages"] = []
+        for i in range(len(all_messages)):
+            st.session_state.messages.append(
+                {
+                    "role": believer_image if i%2 == 0 else disagreer_image,
+                    "content": all_messages[i]["content"]
+                })
+
+    
+    # Starting from 3rd message because the first two are internal
+    # prompts for the agents
+    for i in range(len(st.session_state["messages"][2:])):
+        st.chat_message(believer_image if i%2 == 0 else disagreer_image).write(st.session_state.messages[i]["content"])
+
+    for i in range(5):
+    # while continue_conversation:
         reply_believer = believer.generate_response(all_messages)
-        print(f"Believer: {reply_believer}")
-        print("")
-        
-        time.sleep(3)
+        st.chat_message(believer_image).write(reply_believer)
 
-        reply_disagreer = disagreer.generate_response(all_messages)
-        print(f"Disagreer: {reply_disagreer}")
-        print("")
+        time.sleep(5)
 
-        time.sleep(3)
+        reply_disagreer = believer.generate_response(all_messages)
+        st.chat_message(disagreer_image).write(reply_disagreer)
+
+        time.sleep(5)
+
+        continue_conversation = False
+        # st.button("Continue Conversation")
+
 
 if __name__ == "__main__":
     main()
